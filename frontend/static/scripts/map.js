e4ae4a39124d273c86ba686a3e15364bbf65cd94
Niklas Begley <niklas.begley@gmail.com>
2015-09-13 11:16:34 +0100

    Added some pins to the map

--- frontend/static/scripts/map.js
+++ frontend/static/scripts/map.js
@@ -4,3 +4,28 @@ var map = new GMaps({
   lng: 19.6669283,
   zoom: 4
 });
+
+map.addMarker({
+  lat: 1.9933772,
+  lng: 19.6669283,
+});
+
+map.addMarker({
+  lat: 6.9333772,
+  lng: 9.6669283,
+});
+
+map.addMarker({
+  lat: 2.9333772,
+  lng: 32.6669283,
+});
+
+map.addMarker({
+  lat: 12.9333772,
+  lng: -12.6669283,
+});
+
+map.addMarker({
+  lat: 13.9333772,
+  lng: -13.6669283,
+});
